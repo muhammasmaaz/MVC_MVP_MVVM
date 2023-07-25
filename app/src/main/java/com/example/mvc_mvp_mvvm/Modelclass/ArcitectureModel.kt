77f463a864378d5f5a333412a0c0cff840c99cb1
m.maazhabib <muhammadmaazsystem@gmail.com>
2023-07-25 10:18:44 +0500

    MVP Architecture another way with one interface in presenter class v3

--- app/src/main/java/com/example/mvc_mvp_mvvm/Modelclass/ArcitectureModel.kt
+++ app/src/main/java/com/example/mvc_mvp_mvvm/Modelclass/ArcitectureModel.kt
@@ -3,7 +3,7 @@ package com.example.mvc_mvp_mvvm.Modelclass
 import com.example.mvc_mvp_mvvm.controller.Controller
 
 class ArcitectureModel(var controller: Controller) {
-    var architecture= "MVC"
+    var architecture= "MVp"
     fun showarchitecture(){
         val mvc=architecture
         sendtocontroller(mvc)
